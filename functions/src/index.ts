d82ee7a50a13cc5d1e7485b1035d5df32e92cfd8
rikbellens <rik.bellens@appsup.be>
2025-07-16 18:27:41 +0200

    feat: allow generic paths in LISTEN_TO param and define full path of QUEUE

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -15,6 +15,6 @@ export const onWrite = functions.database.ref(listenTo)
     let queue = process.env.QUEUE || "firebaseQueue";
     let writeType = change.before.exists() && change.after.exists() ? "update" : !change.after.exists() ? "delete" : "create";
     functions.logger.info("onWrite", `${listenTo}/${change.after.key} ${writeType}`);
-    
-    return change.after.ref.root.child(queue).child(listenTo).push({"id": change.before.key, "timestamp": context.timestamp, "type": writeType});
+
+    return change.after.ref.root.child(queue).push({"id": change.before.key, "timestamp": context.timestamp, "type": writeType, "listenTo": listenTo, "params": context.params});
   });
